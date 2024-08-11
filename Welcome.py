6f6f73c56ae5665f1dfa40f703e16b5cd5bd616a
anlesey <719589237@qq.com>
2024-08-11 19:44:31 +0800

    Initial commit

--- Welcome.py
+++ Welcome.py
@@ -5,4 +5,4 @@ with open('Readme.md', encoding='utf-8') as f:
 
 
 if st.button('来试试看吧', use_container_width = True):
-    st.switch_page("pages/1 test.py")
+    st.switch_page("pages/1 singleton.py")
